f8447f03ab4a1f33530b84c5fdde9f952a193568
Júnior Baum <juniorsbaum@hotmail.com>
2023-01-13 01:49:17 -0300

    realiza consultas de cep e trata os erros usando !=200 e while depois

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import requests
 def get_endereco_por_cep(cep):
     url = f"https://viacep.com.br/ws/{cep}/json"
     response = requests.get(url)
+    if response.status_code != 200:
+        return None
     data = response.json()
 
     endereco = {
@@ -14,10 +16,15 @@ def get_endereco_por_cep(cep):
     }
     return endereco
 
-cep = input("Por favor, digite um CEP: ")
-endereco = get_endereco_por_cep(cep)
-print(f"CEP: {endereco['cep']}")
-print(f"Logradouro: {endereco['logradouro']}")
-print(f"Bairro: {endereco['bairro']}")
-print(f"Localidade: {endereco['localidade']}")
-print(f"UF: {endereco['uf']}")
+while True:
+    cep = input("Por favor, digite um CEP: ")
+    endereco = get_endereco_por_cep(cep)
+    if endereco is None:
+        print("CEP inválido. Por favor, tente novamente.")
+    else:
+        print(f"CEP: {endereco['cep']}")
+        print(f"Logradouro: {endereco['logradouro']}")
+        print(f"Bairro: {endereco['bairro']}")
+        print(f"Localidade: {endereco['localidade']}")
+        print(f"UF: {endereco['uf']}")
+        break
